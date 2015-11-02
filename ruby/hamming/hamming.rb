fc9682d3ae3684f6c33adf211e05124763e8251f
Hector Huertas <hectorhuertas@gmail.com>
2015-11-01 22:32:22 -0700

    ruby-hamming other solution

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,6 +2,7 @@ require 'pry'
 class Hamming
   Hamming::VERSION = 1
 
+  #My solution, pairing with Lovisa
   def self.compute(string1, string2)
     fail ArgumentError if string1.length != string2.length
 
@@ -11,4 +12,14 @@ class Hamming
     end
     distance
   end
+
+  # #Another solution, driven by Lovisa
+  # def self.compute(string1, string2)
+  #   fail ArgumentError if string1.length != string2.length
+  #
+  #   string1.chars.zip(string2.chars).reduce(0) do |sum,pair|
+  #     sum += 1 if pair[0] != pair[1]
+  #     sum
+  #   end
+  # end
 end
