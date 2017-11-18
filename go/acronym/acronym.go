8cc180ff8deede5576fe36e8a526240c003542cc
Hector Huertas <hectorhuertas@gmail.com>
2017-11-18 15:19:19 +0000

    Go: acronym v2

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -3,12 +3,13 @@ package acronym
 import "strings"
 
 func Abbreviate(s string) string {
-	s2 := strings.Replace(s, "-", " ", -1)
-	words := strings.Split(s2, " ")
+	s = strings.Replace(s, "-", " ", -1)
+
+	words := strings.Fields(s)
 
 	var acronym string
-	for _, word := range words {
-		acronym += strings.ToUpper(word[0:1])
+	for _, w := range words {
+		acronym += strings.ToUpper(w[0:1])
 	}
 
 	return acronym
