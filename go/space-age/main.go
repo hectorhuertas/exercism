66078dc32ef28de3c751ace8e77b9137ff683adb
Hector Huertas <hectorhuertas@gmail.com>
2018-01-24 09:08:14 +0000

    Go: space-age

---
+++ go/space-age/main.go
@@ -0,0 +1,20 @@
+package space
+
+type Planet string
+
+const secondsInEarthYear = 31557600
+
+var orbitCoeficient = map[Planet]float64{
+	"Earth":   1,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
+
+func Age(seconds float64, p Planet) float64 {
+	return seconds / (secondsInEarthYear * orbitCoeficient[p])
+}
